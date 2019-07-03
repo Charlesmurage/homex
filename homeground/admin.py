3e38cf902afc412f1971cf23ad96c06321447392
Charles maina <muragemaina09@gmail.com>
2019-07-03 14:33:17 +0300

    added new class to save residents

--- homeground/admin.py
+++ homeground/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Resident,Business,tags
 
 # Register your models here.
+admin.site.register(Resident)
+admin.site.register(Business)
+admin.site.register(tags)
--- homeground/models.py
+++ homeground/models.py
@@ -7,6 +7,7 @@ from django.db import models
 class Resident(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
+    phone_number = models.CharField(max_length =10, blank =True)
 
     def __str__(self):
         return self.first_name
