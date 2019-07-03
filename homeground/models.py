78a9584e351469b60a1d20a493bb60f6f274fa39
Charles maina <muragemaina09@gmail.com>
2019-07-03 12:27:59 +0300

    added models resident

--- homeground/models.py
+++ homeground/models.py
@@ -4,3 +4,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Resident(model.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    
