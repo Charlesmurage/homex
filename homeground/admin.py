2670a7c14ea40b37e77a8f68eb003ee0359ba91d
Charles maina <muragemaina09@gmail.com>
2019-07-03 14:54:32 +0300

    created delete classes

--- homeground/admin.py
+++ homeground/admin.py
@@ -4,7 +4,9 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from .models import Resident,Business,tags
 
+class BusinessAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
 # Register your models here.
 admin.site.register(Resident)
-admin.site.register(Business)
+admin.site.register(Business,BusinessAdmin)
 admin.site.register(tags)
