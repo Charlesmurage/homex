0fd1b09339450bf4090890a939461df00849bd56
Charles maina <muragemaina09@gmail.com>
2019-07-03 14:18:21 +0300

    created tests for adding residents

--- homeground/tests.py
+++ homeground/tests.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from .models import Resident,Business,tags
 
 # Create your tests here.
+class ResidentTestClass(TestCase):
+    def setUp(self):
+        self.charles= Resident(first_name = 'charles', last_name = 'maina')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.charles,Resident))
