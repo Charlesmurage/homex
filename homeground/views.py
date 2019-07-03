317986710dfcce2b3bec22db7983c22a5c66bae3
Charles maina <muragemaina09@gmail.com>
2019-07-03 15:49:39 +0300

    made content get displayed in home page

--- homeground/views.py
+++ homeground/views.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Business
 
 # Create your views here.
 def welcome(request):
-    return render(request,'home.html')
+    business = Business.objects.all()
+    return render(request,'home.html',{"business":business})
