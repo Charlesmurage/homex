2c639b2923f65296730f57d181ceeb6ba3e68f6c
Charles maina <muragemaina09@gmail.com>
2019-07-03 12:45:04 +0300

    added new models

--- homeground/models.py
+++ homeground/models.py
@@ -4,7 +4,24 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Resident(model.Model):
+class Resident(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
-    
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+class tags(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+
+class Business(models.Model):
+    title = models.CharField(max_length =40)
+    selling = models.CharField(max_length =60)
+    resident = models.ForeignKey(Resident)
+    tags = models.ManyToManyField(tags)    
