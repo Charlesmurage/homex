3a0f3a41110e8549c44c7962969761cb521ee700
Charles maina <muragemaina09@gmail.com>
2019-07-04 09:46:55 +0300

    added registration page and login page

--- homeground/views.py
+++ homeground/views.py
@@ -4,9 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Business
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 def welcome(request):
     business = Business.objects.all()
-    
     return render(request,'home.html',{"business": business})
+
+@login_required(login_url='/accounts/login/')
+def business(request, business_id):
+    
+    return render(request,'home.html')
